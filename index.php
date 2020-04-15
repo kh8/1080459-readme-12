db6fbd3f3cf9190ee7514a722d9c624aec3c49e8
kh8 <mihailk1@ya.ru>
2020-04-15 21:40:13 +0500

    php queries

--- index.php
+++ index.php
@@ -3,43 +3,43 @@
 $user_name = 'Михаил'; // укажите здесь ваше имя
 $title = 'ReadMe';
 $is_auth = rand(0,1);
-$cards = [
-    [
-        'title' => 'Цитата',
-        'type' => 'post-quote',
-        'content' => 'Мы в жизни любим только раз, а после ищем лишь похожих',
-        'author' => 'Лариса',
-        'avatar' => 'userpic-larisa-small.jpg'
-    ],
-    [
-        'title' => 'Игра престолов',
-        'type' => 'post-text',
-        'content' => 'Не могу дождаться начала финального сезона своего любимого сериала!',
-        'author' => 'Владик',
-        'avatar' => 'userpic.jpg'
-    ],
-    [
-        'title' => 'Наконец, обработал фотки!',
-        'type' => 'post-photo',
-        'content' => 'rock-medium.jpg',
-        'author' => 'Виктор',
-        'avatar' => 'userpic-mark.jpg'
-    ],
-    [
-        'title' => 'Моя мечта',
-        'type' => 'post-photo',
-        'content' => 'coast-medium.jpg',
-        'author' => 'Лариса',
-        'avatar' => 'userpic-larisa-small.jpg'
-    ],
-    [
-        'title' => 'Лучшие курсы',
-        'type' => 'post-link',
-        'content' => 'www.htmlacademy.ru',
-        'author' => 'Владик',
-        'avatar' => 'userpic.jpg'
-    ],
-];
+// $cards = [
+//     [
+//         'title' => 'Цитата',
+//         'type' => 'post-quote',
+//         'content' => 'Мы в жизни любим только раз, а после ищем лишь похожих',
+//         'author' => 'Лариса',
+//         'avatar' => 'userpic-larisa-small.jpg'
+//     ],
+//     [
+//         'title' => 'Игра престолов',
+//         'type' => 'post-text',
+//         'content' => 'Не могу дождаться начала финального сезона своего любимого сериала!',
+//         'author' => 'Владик',
+//         'avatar' => 'userpic.jpg'
+//     ],
+//     [
+//         'title' => 'Наконец, обработал фотки!',
+//         'type' => 'post-photo',
+//         'content' => 'rock-medium.jpg',
+//         'author' => 'Виктор',
+//         'avatar' => 'userpic-mark.jpg'
+//     ],
+//     [
+//         'title' => 'Моя мечта',
+//         'type' => 'post-photo',
+//         'content' => 'coast-medium.jpg',
+//         'author' => 'Лариса',
+//         'avatar' => 'userpic-larisa-small.jpg'
+//     ],
+//     [
+//         'title' => 'Лучшие курсы',
+//         'type' => 'post-link',
+//         'content' => 'www.htmlacademy.ru',
+//         'author' => 'Владик',
+//         'avatar' => 'userpic.jpg'
+//     ],
+// ];
 
 require_once('helpers.php');
 require_once('functions.php');
--- templates/main.php
+++ templates/main.php
@@ -1,177 +1,169 @@
 <div class="container">
-        <h1 class="page__title page__title--popular">Популярное</h1>
-    </div>
-    <div class="popular container">
-        <div class="popular__filters-wrapper">
-            <div class="popular__sorting sorting">
-                <b class="popular__sorting-caption sorting__caption">Сортировка:</b>
-                <ul class="popular__sorting-list sorting__list">
-                    <li class="sorting__item sorting__item--popular">
-                        <a class="sorting__link sorting__link--active" href="#">
-                            <span>Популярность</span>
-                            <svg class="sorting__icon" width="10" height="12">
-                                <use xlink:href="#icon-sort"></use>
-                            </svg>
-                        </a>
-                    </li>
-                    <li class="sorting__item">
-                        <a class="sorting__link" href="#">
-                            <span>Лайки</span>
-                            <svg class="sorting__icon" width="10" height="12">
-                                <use xlink:href="#icon-sort"></use>
-                            </svg>
-                        </a>
-                    </li>
-                    <li class="sorting__item">
-                        <a class="sorting__link" href="#">
-                            <span>Дата</span>
-                            <svg class="sorting__icon" width="10" height="12">
-                                <use xlink:href="#icon-sort"></use>
-                            </svg>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <div class="popular__filters filters">
-                <b class="popular__filters-caption filters__caption">Тип контента:</b>
-                <ul class="popular__filters-list filters__list">
-                    <li class="popular__filters-item popular__filters-item--all filters__item filters__item--all">
-                        <a class="filters__button filters__button--ellipse filters__button--all filters__button--active" href="#">
-                            <span>Все</span>
-                        </a>
-                    </li>
-                    <li class="popular__filters-item filters__item">
-                        <a class="filters__button filters__button--photo button" href="#">
-                            <span class="visually-hidden">Фото</span>
-                            <svg class="filters__icon" width="22" height="18">
-                                <use xlink:href="#icon-filter-photo"></use>
-                            </svg>
-                        </a>
-                    </li>
-                    <li class="popular__filters-item filters__item">
-                        <a class="filters__button filters__button--video button" href="#">
-                            <span class="visually-hidden">Видео</span>
-                            <svg class="filters__icon" width="24" height="16">
-                                <use xlink:href="#icon-filter-video"></use>
-                            </svg>
-                        </a>
-                    </li>
-                    <li class="popular__filters-item filters__item">
-                        <a class="filters__button filters__button--text button" href="#">
-                            <span class="visually-hidden">Текст</span>
-                            <svg class="filters__icon" width="20" height="21">
-                                <use xlink:href="#icon-filter-text"></use>
-                            </svg>
-                        </a>
-                    </li>
-                    <li class="popular__filters-item filters__item">
-                        <a class="filters__button filters__button--quote button" href="#">
-                            <span class="visually-hidden">Цитата</span>
-                            <svg class="filters__icon" width="21" height="20">
-                                <use xlink:href="#icon-filter-quote"></use>
-                            </svg>
-                        </a>
-                    </li>
-                    <li class="popular__filters-item filters__item">
-                        <a class="filters__button filters__button--link button" href="#">
-                            <span class="visually-hidden">Ссылка</span>
-                            <svg class="filters__icon" width="21" height="18">
-                                <use xlink:href="#icon-filter-link"></use>
-                            </svg>
-                        </a>
-                    </li>
-                </ul>
-            </div>
+    <h1 class="page__title page__title--popular">Популярное</h1>
+</div>
+<?php
+    $con = mysqli_connect("localhost", "root", "", "readme");
+    if ($con == false) {
+        $error = mysqli_connect_error();
+    } else {
+        mysqli_set_charset($con, "utf8");
+        $sql = "SELECT users.username, users.avatar, posts.title, posts.content, posts.view_count, content_types.type_class FROM posts INNER JOIN users ON posts.author_id=users.id INNER JOIN content_types ON posts.post_type=content_types.id ORDER  BY view_count DESC;";
+        $result = mysqli_query($con, $sql);
+        if (!$result) {
+            $error = mysqli_error($con);
+        } else {
+            $cards = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        }
+        $sql = "SELECT * FROM content_types;";
+        $result = mysqli_query($con, $sql);
+        if (!$result) {
+            $error = mysqli_error($con);
+        } else {
+            $content_types = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        }
+    }
+?>
+<div class="popular container">
+    <div class="popular__filters-wrapper">
+        <div class="popular__sorting sorting">
+            <b class="popular__sorting-caption sorting__caption">Сортировка:</b>
+            <ul class="popular__sorting-list sorting__list">
+                <li class="sorting__item sorting__item--popular">
+                    <a class="sorting__link sorting__link--active" href="#">
+                        <span>Популярность</span>
+                        <svg class="sorting__icon" width="10" height="12">
+                            <use xlink:href="#icon-sort"></use>
+                        </svg>
+                    </a>
+                </li>
+                <li class="sorting__item">
+                    <a class="sorting__link" href="#">
+                        <span>Лайки</span>
+                        <svg class="sorting__icon" width="10" height="12">
+                            <use xlink:href="#icon-sort"></use>
+                        </svg>
+                    </a>
+                </li>
+                <li class="sorting__item">
+                    <a class="sorting__link" href="#">
+                        <span>Дата</span>
+                        <svg class="sorting__icon" width="10" height="12">
+                            <use xlink:href="#icon-sort"></use>
+                        </svg>
+                    </a>
+                </li>
+            </ul>
         </div>
-        <div class="popular__posts">
-            <?php foreach($cards as $index => $card): ?>
-                <article class="popular__post post <?=$card['type']?>">
-                <header class="post__header">
-                    <h2><?=$card['title']?></h2>
-                </header>
-                <div class="post__main">
-                    <?php switch($card['type']): case 'post-quote': ?>
-                        <blockquote>
-                            <p><?=htmlspecialchars($card['content']) ?></p>
-                            <cite>Неизвестный Автор</cite>
-                        </blockquote>
-                    <?php break; case 'post-text': ?>
-                        <p><?=htmlspecialchars(truncate_text($card['content']))?></p>
-                        <?php if (mb_strlen($card['content']) > 300):?>
-                            <a class="post-text__more-link" href="#">Читать далее</a>
-                        <?php endif; ?>
-                    <?php break; case 'post-photo': ?>
-                        <div class="post-photo__image-wrapper">
-                            <img src="img/<?=$card['content'];?>" alt="Фото от пользователя" width="360" height="240">
-                        </div>
-                    <?php break; case 'post-link': ?>
-                        <div class="post-link__wrapper">
-                            <a class="post-link__external" href="http://" title="Перейти по ссылке">
-                                <div class="post-link__info-wrapper">
-                                    <div class="post-link__icon-wrapper">
-                                        <img src="https://www.google.com/s2/favicons?domain=vitadental.ru" alt="Иконка">
-                                    </div>
-                                    <div class="post-link__info">
-                                        <h3><?=$card['title'];?></h3>
-                                    </div>
+        <div class="popular__filters filters">
+            <b class="popular__filters-caption filters__caption">Тип контента:</b>
+            <ul class="popular__filters-list filters__list">
+                <li class="popular__filters-item popular__filters-item--all filters__item filters__item--all">
+                    <a class="filters__button filters__button--ellipse filters__button--all filters__button--active" href="#">
+                        <span>Все</span>
+                    </a>
+                </li>
+                <?php foreach($content_types as $index => $content_type): ?>
+                    <li class="popular__filters-item filters__item">
+                        <a class="filters__button filters__button--<?=$content_type['type_class']?> button" href="#">
+                            <span class="visually-hidden"><?=$content_type['type_name'];?></span>
+                            <svg class="filters__icon" width="22" height="21">
+                                <use xlink:href="#icon-filter-<?=$content_type['type_class']?>"></use>
+                            </svg>
+                        </a>
+                    </li>
+                <?php endforeach ?>
+            </ul>
+        </div>
+    </div>
+    <div class="popular__posts">
+        <?php foreach($cards as $index => $card): ?>
+            <article class="popular__post post post-<?=$card['type_class']?>">
+            <header class="post__header">
+                <h2><?=$card['title']?></h2>
+            </header>
+            <div class="post__main">
+                <?php switch($card['type_class']): case 'quote': ?>
+                    <blockquote>
+                        <p><?=htmlspecialchars($card['content']) ?></p>
+                        <cite>Неизвестный Автор</cite>
+                    </blockquote>
+                <?php break; case 'text': ?>
+                    <p><?=htmlspecialchars(truncate_text($card['content']))?></p>
+                    <?php if (mb_strlen($card['content']) > 300):?>
+                        <a class="post-text__more-link" href="#">Читать далее</a>
+                    <?php endif; ?>
+                <?php break; case 'photo': ?>
+                    <div class="post-photo__image-wrapper">
+                        <img src="img/<?=$card['content'];?>" alt="Фото от пользователя" width="360" height="240">
+                    </div>
+                <?php break; case 'link': ?>
+                    <div class="post-link__wrapper">
+                        <a class="post-link__external" href="http://" title="Перейти по ссылке">
+                            <div class="post-link__info-wrapper">
+                                <div class="post-link__icon-wrapper">
+                                    <img src="https://www.google.com/s2/favicons?domain=vitadental.ru" alt="Иконка">
                                 </div>
-                                <span><?=htmlspecialchars($card['content'])?></span>
-                            </a>
-                        </div>
-                    <?php break; case 'post-video': ?>
-                        <div class="post-video__block">
-                        <div class="post-video__preview">
-                            <?
-                            // =embed_youtube_cover(/* вставьте ссылку на видео */);
-                            ?>
-                            <img src="img/coast-medium.jpg" alt="Превью к видео" width="360" height="188">
+                                <div class="post-link__info">
+                                    <h3><?=$card['title'];?></h3>
+                                </div>
                             </div>
-                            <a href="post-details.html" class="post-video__play-big button">
-                                <svg class="post-video__play-big-icon" width="14" height="14">
-                                    <use xlink:href="#icon-video-play-big"></use>
-                                </svg>
-                                <span class="visually-hidden">Запустить проигрыватель</span>
-                            </a>
-                        </div>
-                    <?php endswitch ?>
-                </div>
-                <footer class="post__footer">
-                    <div class="post__author">
-                        <a class="post__author-link" href="#" title="Автор">
-                            <div class="post__avatar-wrapper">
-                                <!--укажите путь к файлу аватара-->
-                                <img class="post__author-avatar" src="img/<?=$card['avatar']?>" alt="Аватар пользователя">
-                            </div>
-                            <div class="post__info">
-                                <b class="post__author-name"><?=$card['author']?></b>
-                                <?php $post_time = get_post_time($index) ?>
-                                <time class="post__time" datetime="<?= $post_time->format('Y-m-d H:i:s') ?>" title="<?= $post_time->format('d.m.Y H:i') ?>"><?= absolute_time_to_relative($post_time) ?></time>
-                            </div>
+                            <span><?=htmlspecialchars($card['content'])?></span>
                         </a>
                     </div>
-                    <div class="post__indicators">
-                        <div class="post__buttons">
-                            <a class="post__indicator post__indicator--likes button" href="#" title="Лайк">
-                                <svg class="post__indicator-icon" width="20" height="17">
-                                    <use xlink:href="#icon-heart"></use>
-                                </svg>
-                                <svg class="post__indicator-icon post__indicator-icon--like-active" width="20" height="17">
-                                    <use xlink:href="#icon-heart-active"></use>
-                                </svg>
-                                <span>0</span>
-                                <span class="visually-hidden">количество лайков</span>
-                            </a>
-                            <a class="post__indicator post__indicator--comments button" href="#" title="Комментарии">
-                                <svg class="post__indicator-icon" width="19" height="17">
-                                    <use xlink:href="#icon-comment"></use>
-                                </svg>
-                                <span>0</span>
-                                <span class="visually-hidden">количество комментариев</span>
-                            </a>
+                <?php break; case 'video': ?>
+                    <div class="post-video__block">
+                    <div class="post-video__preview">
+                        <?
+                        // =embed_youtube_cover(/* вставьте ссылку на видео */);
+                        ?>
+                        <img src="img/coast-medium.jpg" alt="Превью к видео" width="360" height="188">
                         </div>
+                        <a href="post-details.html" class="post-video__play-big button">
+                            <svg class="post-video__play-big-icon" width="14" height="14">
+                                <use xlink:href="#icon-video-play-big"></use>
+                            </svg>
+                            <span class="visually-hidden">Запустить проигрыватель</span>
+                        </a>
                     </div>
-                </footer>
-                </article>
-            <?php endforeach; ?>
-        </div>
+                <?php endswitch ?>
+            </div>
+            <footer class="post__footer">
+                <div class="post__author">
+                    <a class="post__author-link" href="#" title="Автор">
+                        <div class="post__avatar-wrapper">
+                            <!--укажите путь к файлу аватара-->
+                            <img class="post__author-avatar" src="img/<?=$card['avatar']?>" alt="Аватар пользователя">
+                        </div>
+                        <div class="post__info">
+                            <b class="post__author-name"><?=$card['username']?></b>
+                            <?php $post_time = get_post_time($index) ?>
+                            <time class="post__time" datetime="<?= $post_time->format('Y-m-d H:i:s') ?>" title="<?= $post_time->format('d.m.Y H:i') ?>"><?= absolute_time_to_relative($post_time) ?></time>
+                        </div>
+                    </a>
+                </div>
+                <div class="post__indicators">
+                    <div class="post__buttons">
+                        <a class="post__indicator post__indicator--likes button" href="#" title="Лайк">
+                            <svg class="post__indicator-icon" width="20" height="17">
+                                <use xlink:href="#icon-heart"></use>
+                            </svg>
+                            <svg class="post__indicator-icon post__indicator-icon--like-active" width="20" height="17">
+                                <use xlink:href="#icon-heart-active"></use>
+                            </svg>
+                            <span>0</span>
+                            <span class="visually-hidden">количество лайков</span>
+                        </a>
+                        <a class="post__indicator post__indicator--comments button" href="#" title="Комментарии">
+                            <svg class="post__indicator-icon" width="19" height="17">
+                                <use xlink:href="#icon-comment"></use>
+                            </svg>
+                            <span>0</span>
+                            <span class="visually-hidden">количество комментариев</span>
+                        </a>
+                    </div>
+                </div>
+            </footer>
+            </article>
+        <?php endforeach; ?>
     </div>
+</div>
